a33d4cdb16d40286f1de4a3580c924ad8088618c
rockmtn <rockmtn@protonmail.com>
2019-08-01 17:42:07 +0700

    align to discord creation

--- member_stats.rb
+++ member_stats.rb
@@ -8,6 +8,7 @@ require "./discord_api"
 ZXB_GUILD_ID    = "412477591778492427"
 DAYS_PER_BIN    = 7 # set to 7 for weekly stats
 SECONDS_PER_BIN = DAYS_PER_BIN * 86400
+START_BIN       = Time.parse("2018-02-12 00:00:00 +0000").to_i # day of 0xBitcoin Discord creation
 FILENAME        = "members.tsv"
 
 load_config!
@@ -28,12 +29,12 @@ puts "got #{users.count} total users"
 bins = Hash.new { |h, k| h[k] = [] }
 users.each do |u|
   joined_at = Time.parse(u[:joined_at])
-  bin = joined_at.to_i / SECONDS_PER_BIN * SECONDS_PER_BIN
+  bin = (joined_at.to_i - START_BIN) / SECONDS_PER_BIN * SECONDS_PER_BIN + START_BIN
   bins[bin] << u
 end
-bins = bins.sort_by { |k, v| k }.
-  tap { |bins| bins.pop }.  # ignore last week of results since it's not over yet
-  to_h
+bins =
+  bins.sort.
+  tap { |bins| bins.pop } # ignore last week of results since it's not over yet
 
 total = 0
 File.open(FILENAME, "w") do |o|
